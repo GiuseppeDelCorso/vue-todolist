b592a6a974ea0a8ce4115ad14c22e23728bad81f
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-17 16:07:02 +0100

    sv

--- main.js
+++ main.js
@@ -4,25 +4,26 @@ const { createApp } = Vue;
 const lista = {
     data: function () {
         return {
-            
+            barratura: false,
             index: 0,
             listaAzioni:[
 
                 {
                     text: "Accendere il Computer",
+                    flag: false,
                    
                 },
                 {
                     text: "Aprire Il Broswer",
-
+                    flag: false,
                 },
                 {
                     text: "Digitare Youtube",
-
+                    flag: false,
                 },
                 {
                     text: "Segliere Video",
-
+                    flag: false,
                 },
 
                 
@@ -34,7 +35,7 @@ const lista = {
     methods: {
         fatto(){
             
-            
+            this.barratura = true
             console.log("fatto")
            
         }
